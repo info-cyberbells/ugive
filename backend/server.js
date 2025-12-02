4d77b1e88d2f06233ca9b7dd67c45158dc712e59
Prince Patial <princepatial20@gmail.com>
2025-12-02 18:05:20 +0530

    server.js fie

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import superAdminRoutes from "./src/routes/super-admin.routes.js";
@@ -9,24 +11,29 @@ import studentRoutes from "./src/routes/student.routes.js";
 import publicRoutes from "./src/routes/public.routes.js";
 import searchRoutes from "./src/routes/search.routes.js";
 
-dotenv.config();
 connectDB();
 
 const app = express();
 
+// Middleware
+app.set('trust proxy', true);
+app.use(cookieParser());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
 }));
-app.use(express.json());
 
-// Mount routes with role-based prefixes
+// Static files
+app.use("/uploads", express.static("uploads"));
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/super_admin", superAdminRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/student", studentRoutes);
 app.use("/api/public", publicRoutes);
-app.use("/uploads", express.static("uploads"));
 app.use("/api/search", searchRoutes);
 
 const PORT = process.env.PORT || 5000;
